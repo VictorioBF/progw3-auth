44005ca4c07d65bbc3b240f8ef55c0e3eb1b2ec0
VictorioBF <victoriofaraco@gmail.com>
2021-12-07 11:48:37 -0300

    model passo 1 B)

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+class Usuario extends Model implements Authenticatable
 {
     use HasFactory;
 
