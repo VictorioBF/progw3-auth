1357c03f2d77e287df84bf3d5dcaea2dff422fe5
VictorioBF <victoriofaraco@gmail.com>
2021-12-09 10:33:29 -0300

    model update

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,6 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model implements Authenticatable
 {
+    function getAuthIdentifierName(){
+        return 'id';
+    }
+    
+    function getAuthIdentifier(){
+        return $this->id;
+    }
+
+    function getAuthPassword(){
+        return $this->password;
+    }
+
+    function getRememberToken(){
+        return;
+    }
+    
+    function setRememberToken($data){
+        return;
+    }
+
+    function getRememberTokenName(){
+        return 'token';
+    }
+
     use HasFactory;
 
     public $timestamps = false;
